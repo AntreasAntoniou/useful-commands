09b3e24c85cc698133a646b2cfbe62e637c01435
Antreas Antoniou <antrikohs@gmail.com>
2022-10-08 20:19:02 +0100

    Update setup-dev-env.sh

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -1,31 +1,35 @@
-apt install micro bat git tmux fish
-
-curl -sS https://starship.rs/install.sh | sh
-eval "$(starship init bash)"
-starship init fish | source
-
 ```bash
-cd
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
+apt install micro bat git tmux fish
 ```
 
 ```bash
-apt update
-apt install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+curl -sS https://starship.rs/install.sh | sh
+eval "$(starship init bash)"
+starship init fish | source
+```
 
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+```bash
+    cd
+    git clone https://github.com/gpakosz/.tmux.git
+    ln -s -f .tmux/.tmux.conf
+    cp .tmux/.tmux.conf.local .
+```
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+```bash
+    apt update
+    apt install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
 
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+      $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    apt-get update
+    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 ```
