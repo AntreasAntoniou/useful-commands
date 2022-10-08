b3c6039dcfd54056529411046815817cf29f60e8
Antreas Antoniou <antrikohs@gmail.com>
2022-10-08 20:18:15 +0100

    Update setup-dev-env.sh

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -4,7 +4,28 @@ curl -sS https://starship.rs/install.sh | sh
 eval "$(starship init bash)"
 starship init fish | source
 
+```bash
 cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
+```
+
+```bash
+apt update
+apt install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+```
